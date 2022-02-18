9e9ebc6663d19a9e1284772a005b611333693c72
Mikhawa <michaeljpitz@gmail.com>
2022-02-18 15:42:16 +0100

    replace

--- controller/publicThearticleController.php
+++ controller/publicThearticleController.php
@@ -9,9 +9,7 @@
 //require_once "../view/publicView/publicHomepageView.php";
 
 if (isset($_GET['connect'])) {
-    require_once "../view/publicView/publicConnectView.php";}
-
-else {
+    require_once "../view/publicView/publicConnectView.php";
+} else {
     require_once "../view/publicView/publicHomepageView.php";
-    }
-
+}
