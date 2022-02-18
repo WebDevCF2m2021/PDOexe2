7369c44192b585067974bac9a82bc77bab4be706
saber <sabersab32gmail.com>
2022-02-18 11:02:31 +0100

    modification du menu ok

--- view/publicView/publicHomepageView.php
+++ view/publicView/publicHomepageView.php
@@ -29,6 +29,16 @@
         <li class="nav-item">
           <a class="nav-link" href="#">About</a>
         </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Section</a>
+          <div class="dropdown-menu">
+            <a class="dropdown-item" href="#">Action</a>
+            <a class="dropdown-item" href="#">Another action</a>
+            <a class="dropdown-item" href="#">Something else here</a>
+            <div class="dropdown-divider"></div>
+            <a class="dropdown-item" href="#">Separated link</a>
+          </div>
+        </li>
       </ul>
       <ul class="navbar-nav ms-md-auto">
 
