a884abdc02fadea4cf6f51ab2e4538aca8208a53
Bilel-J <bileljaha2@gmail.com>
2022-02-18 11:00:14 +0100

    essaye4

--- model/theSectionModel.php
+++ model/theSectionModel.php
@@ -4,7 +4,8 @@ try{
 $db = $theSectionSelectAllNav -> query 
     ("SELECT * idthesection,thesectiontitle 
       FROM thesection
-      ORDER BY thesectiontitle ASC") -> fetchall();
+      ORDER BY thesectiontitle ASC") ;
+     $result = $sth->fetchAll(); 
 }catch (Exception $e){
     echo $tab. $e->getMessage();
 }
