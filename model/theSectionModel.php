5b5044ba4db72a84240aa6f8037163a17ec482be
thomas-ayissi <thomas8sea@gmail.com>
2022-02-18 14:17:49 +0100

    /*Ticket Thomas : créer une fonction nommée thesectionSelectOneById(PDO $db, int $id) Elle va charger tous les champs de la table thesection quand l'idthesection vaut $id (1 résultat ou 0), Requête préparée, renvoie un tableau associatif si réussi, un tableau vide en cas d'échec*/

--- model/theSectionModel.php
+++ model/theSectionModel.php
@@ -12,3 +12,20 @@ function theSectionSelectAllNav(PDO $db)
     }
     return $result;
 }
+/*Ticket Thomas : créer une fonction nommée thesectionSelectOneById(PDO $db, int $id)
+Elle va charger tous les champs de la table thesection quand l'idthesection vaut $id (1 résultat ou 0),
+Requête préparée, renvoie un tableau associatif si réussi, un tableau vide en cas d'échec*/
+
+
+function thesectionSelectOneById(PDO $db, int $id)
+{
+    try {
+        $sth = $db->query("SELECT * 
+              FROM thesection
+              WHERE idsection BETWEEN 0 and 1");
+        $result = $sth->fetch(PDO::FETCH_ASSOC);
+    } catch (Exception $e) {
+        return [];
+    }
+    return $result;
+}
