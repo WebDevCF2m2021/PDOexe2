53cc6e1ecabbd2486f0c9ba7e1feb7c9ea185646
Mikhawa <michaeljpitz@gmail.com>
2022-02-18 15:13:49 +0100

    debug connect

--- view/publicView/publicConnectView.php
+++ view/publicView/publicConnectView.php
@@ -2,24 +2,15 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Connexion</title>
-    <link rel="stylesheet" href="css/bootstrap.min.css">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Connexion</title>
+  <link rel="stylesheet" href="css/bootstrap.min.css">
 </head>
+
 <body>
-<<<<<<< HEAD
-                   <div>
-                        <a class="nav-link" href="?connect">Connexion</a>
-</div>
-                    
-                    <p class="lead">Veuillez vous connecter</p>
 
-                    <hr>
-                    <div>
-=======
-
-<div class="navbar navbar-expand-lg sticky-top navbar-light bg-light">
+  <div class="navbar navbar-expand-lg sticky-top navbar-light bg-light">
     <div class="container">
       <a href="./" class="navbar-brand">Accueil</a>
       <ul class="navbar-nav me-auto">
@@ -40,49 +31,48 @@
       </ul>
       <ul class="navbar-nav ms-md-auto">
 
-       
+
       </ul>
     </div>
   </div>
 
-  
-                 
-                    
-                    
-                    <br>
-                    <p class="container">Veuillez vous connecter</p>
-                    <div class="container">
->>>>>>> a7dd0f3d41b38024106238199ed62578bbb10a2a
-                        <?php
-                        if (isset($error)) :
-                        ?>
-                            <div class="alert alert-danger" role="alert">
-                                <?= $error ?>
-                            </div>
-                        <?php
-                        endif;
-                        ?>
-                        <form action="" name="connexion" method="POST">
-                            <div class="form-group">
-                                <label for="exampleInputEmail1">Votre login</label>
-                                <input type="text" name="theuserLogin" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" required>
-                                <small id="emailHelp" class="form-text text-muted">Login incorrect</small>
-                            </div>
-                            <div class="form-group">
-                                <label for="exampleInputPassword1">Votre mot de passe</label>
-                                <input type="password" name="theuserPwd" class="form-control" id="exampleInputPassword1" required>
-                            </div>
-                            <button type="submit" class="btn btn-primary">Se connecter</button>
-                        </form>
-                    </div>
-                
-                </div>
 
-            </div>
-        </div>
 
-        <script src="js/bootstrap.bundle.min.js"></script>
-        <script src="js/jquery.min.js"></script>
+
+
+  <br>
+  <p class="container">Veuillez vous connecter</p>
+  <div class="container">
+    <?php
+    if (isset($error)) :
+    ?>
+      <div class="alert alert-danger" role="alert">
+        <?= $error ?>
+      </div>
+    <?php
+    endif;
+    ?>
+    <form action="" name="connexion" method="POST">
+      <div class="form-group">
+        <label for="exampleInputEmail1">Votre login</label>
+        <input type="text" name="theuserLogin" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" required>
+        <small id="emailHelp" class="form-text text-muted">Login incorrect</small>
+      </div>
+      <div class="form-group">
+        <label for="exampleInputPassword1">Votre mot de passe</label>
+        <input type="password" name="theuserPwd" class="form-control" id="exampleInputPassword1" required>
+      </div>
+      <button type="submit" class="btn btn-primary">Se connecter</button>
+    </form>
+  </div>
+
+  </div>
+
+  </div>
+  </div>
+
+  <script src="js/bootstrap.bundle.min.js"></script>
+  <script src="js/jquery.min.js"></script>
 </body>
 
 </html>
