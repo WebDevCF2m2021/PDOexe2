8e6992b919105291f0189d5693bb069b91d54808
24manu <manuel.mouzelard@hotmail.com>
2022-02-18 11:09:07 +0100

    controller connect

--- controller/publicThearticleController.php
+++ controller/publicThearticleController.php
@@ -5,4 +5,11 @@
  * Public Homepage
  */
 
-require_once "../view/publicView/publicHomepageView.php";
+//require_once "../view/publicView/publicHomepageView.php";
+
+if (isset($_GET['?connect'])) {
+    require_once "../view/publicView/publicConnectView.php";}
+
+else {
+    require_once "../view/publicView/publicHomepageView.php";
+    }
