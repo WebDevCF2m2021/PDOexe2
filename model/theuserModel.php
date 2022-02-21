b96f45360d076fab78d6926cc79981f3cb036f20
SimonChambon <Simon.CHAMBON@cf2000.onmicrosoft.com>
2022-02-21 11:49:14 +0100

    modification PDO

--- model/theuserModel.php
+++ model/theuserModel.php
@@ -1,9 +1,13 @@
 <?php
 
-function theuserSelectAll(mysqli $db): array
+function theuserSelectAll(PDO $db): array
 {
-    $sql = "SELECT idtheuser, theuserLogin
-            FROM theuser ORDER BY theuserName ASC;";
-    $request = mysqli_query($db, $sql) or die("Erreur SQL :" . mysqli_error($db));
-    return mysqli_fetch_all($request, MYSQLI_ASSOC);
+    try {
+        $sth = $db->query("SELECT idtheuser, theuserLogin
+        FROM theuser ORDER BY theuserName ASC");
+        $result = $sth->fetchAll(PDO::FETCH_ASSOC);
+    } catch (Exception $e) {
+        return [];
+    }
+    return $result;
 }
