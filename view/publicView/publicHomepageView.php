176fbf3a1830fc3524ef72613087118b16c41c4c
cahitYes <efeyesiloglu@hotmail.com>
2022-02-21 10:56:14 +0100

    mise a jour

--- view/publicView/publicHomepageView.php
+++ view/publicView/publicHomepageView.php
@@ -6,7 +6,7 @@
   <title>Bootswatch: Lux</title>
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="css/bootstrap.min.css">
-  <link rel="stylesheet" href="css/custom.min.css">
+
 
 </head>
 
