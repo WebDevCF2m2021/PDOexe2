8de28030cc51aac5731cc34089da189dc4383b9a
Bilel-J <bileljaha2@gmail.com>
2022-03-03 10:09:35 +0100

    pause

--- view/publicView/publicDetailArticle.php
+++ view/publicView/publicDetailArticle.php
@@ -1,2 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
 
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>DetailArticle</title>
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <link rel="stylesheet" href="css/custom.min.css">
+</head>
+
+<body>
+
+  <div class="navbar navbar-expand-lg sticky-top navbar-light bg-light">
+    <div class="container">
+      <a href="./" class="navbar-brand">Accueil</a>
+      <ul class="navbar-nav me-auto">
+        <li class="nav-item">
+          <a class="nav-link active" href="./">Home
+            <span class="visually-hidden">(current)</span>
+          </a>
+        </li>
+        <li class="nav-item dropdown show">
+          <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Section</a>
+          <div class="dropdown-menu" aria-labelledby="themes">
+            <?php
+            foreach ($sections as $section) {
+            ?>
+              <a class="dropdown-item" href="?idsection=<?= $section['idthesection'] ?>"><?= $section['thesectiontitle'] ?></a>
+            <?php
+            }
+            ?>
+            <div class="dropdown-divider"></div>
+          </div>
+        </li>
+      </ul>
+    </div>
+  </div>
+
+
+</body>
+</html>
  <?php
+
+<?php
+  include '../view/footer.php';
+  ?>
